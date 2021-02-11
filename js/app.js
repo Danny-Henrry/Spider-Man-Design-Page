82fda1efa9d2237e276904d35ab48f7e73f17656
Danny-Henrry <78286885+Danny-Henrry@users.noreply.github.com>
2021-02-11 14:05:10 -0300

    adição dos efeitos de Timeline
    efeitos de Timeline e javascript.

--- js/app.js
+++ js/app.js
@@ -14,4 +14,26 @@ window.onload = ()=>{
             },0)
     })
 
+    let tl = new TimelineMax();
+
+    tl.fromTo(".bg-loader", 1, {width: '100%'}, {width: '0%', delay: 3, ease: Expo.easeInOut});
+
+    tl.fromTo(".bg-video", 2, {width: '0%', opacity: 0}, {width: '100%', opacity: 1, ease: Expo.easeInOut},'-=1');
+
+    tl.fromTo(".logo", 0.7, {y: -50, opacity: 0}, {y: 0, opacity: 1, ease: Expo.easeInOut},'-=1');
+
+    tl.fromTo(".nav-list", 0.7, {y: -50, opacity: 0}, {y: 0, opacity: 1, ease: Expo.easeInOut},'-=0.5');
+
+    tl.fromTo(".nav-social", 0.7, {y: -50, opacity: 0}, {y: 0, opacity: 1, ease: Expo.easeInOut},'-=0.5');
+
+    tl.fromTo(".item-1", 0.7, {y: -50, opacity: 0}, {y: 0, opacity: 1, ease: Expo.easeInOut},'-=0.5');
+
+    tl.fromTo(".item-2", 0.7, {y: -50, opacity: 0}, {y: 0, opacity: 1, ease: Expo.easeInOut},'-=0.5');
+
+    tl.fromTo(".item-3", 0.7, {y: -50, opacity: 0}, {y: 0, opacity: 1, ease: Expo.easeInOut},'-=0.5');
+
+    tl.fromTo(".item-4", 0.7, {y: 50, opacity: 0}, {y: 0, opacity: 1, ease: Expo.easeInOut},'-=0.5');
+
+    tl.fromTo(".item-img", 0.7, {y: 50, x: -30, opacity: 0}, {y: 0, x: 0, opacity: 1, ease: Expo.easeInOut},'-=0.2');
+
 }
